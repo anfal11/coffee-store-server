d12b03eb2099f8e0e0be4a3cba54717848729f5f
anfal11 <anfal15-14303@diu.edu.bd>
2023-10-15 14:30:13 +0600

    delete function done in backend

--- index.js
+++ index.js
@@ -66,6 +66,14 @@ async function run() {
             res.send(result);
     })
 
+    // user related api's -> delete user (DELETE)
+    app.delete('/users/:id', async (req, res)=>{
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+  })
+
     app.put('/coffee/:id', async (req, res) => {
           const id = req.params.id;
           const filter = { _id: new ObjectId(id) };
